c29c1d47fcc4dfd83b40b40b5c256aa1a261ec7c
bigwudan <bigwudan@qq.com>
2018-12-07 11:53:48 +0800

    111

--- event.c
+++ event.c
@@ -126,5 +126,27 @@ event_base_priority_init(struct event_base *base, int npriorities)
     return (0);
 }
 
+void
+event_set(struct event *ev, int fd, short events,
+        void (*callback)(int, short, void *), void *arg)
+{
+    /* Take the current base - caller needs to set the real base later */
+    ev->ev_base = current_base;
+
+    ev->ev_callback = callback;
+    ev->ev_arg = arg;
+    ev->ev_fd = fd;
+    ev->ev_events = events;
+    ev->ev_res = 0;
+    ev->ev_flags = EVLIST_INIT;
+    ev->ev_ncalls = 0;
+    ev->ev_pncalls = NULL;
+    //min_heap_elem_init(ev);
+
+    /* by default, we put new events into the middle priority */
+    if(current_base)
+        ev->ev_pri = current_base->nactivequeues/2;
+}
+
 
 
